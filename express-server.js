c86d64d13c850aefc53fcee9d703bcb344d1babd
Nick Lee <nickoless.lee@gmail.com>
2018-01-24 03:58:14 +0000

    Added functionality to display short and long URLs

--- express-server.js
+++ express-server.js
@@ -1,15 +1,18 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080; //default port 8080
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8080; //default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+const urlDatabase = {
    "b2xVn2": "http://lighthouselabs.ca",
    "9sm5xK": "http://www.google.com"
 };
 
 
+// Create: GET
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
@@ -20,13 +23,50 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index", urlDatabase);
+  res.render("urls_index", { urlDatabase:urlDatabase });
+});
+
+// app.get("/urls/:id", (req,res) => {
+//   let shortURL = req.params.id;
+//   let templateVars = {
+//     shortURL: shortURL, 
+//     longURL: urlDatabase[shortURL]
+//   };
+//   res.render("urls_show", templateVars);
+// });
+
+app.get("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls/new", (req,res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.listen(PORT, () =>{
-  console.log(`Example app listening on port ${PORT}!`)
+// Retrieve: POST
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
+
+app.listen(PORT, () =>{
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString(str) {
+  let randomStr = "";
+  const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+  for (let i = 0; i < 6; i++) {
+    emptyStr += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return randomStr;
+}
+
